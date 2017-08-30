220cb5d4e6a460606cff6dcee95749046c88426a
Leonard Chew <leochew@gmail.com>
2017-08-30 06:57:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,12 @@
 def display_board
   title = "A Tic Tac Toe Board"
+  row = "   |   |   "
   dash = "-----------"
   puts title
+  puts row
   puts dash
+  puts row
+  puts dash
+  puts row
 end
 display_board
